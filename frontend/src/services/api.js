9cf28e6e2d0547b2beedb859e8fdbd523684df3e
MujahidKhan870 <khanmujahid7718@gmail.com>
2026-06-06 10:22:58 +0530

    Fix production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://investment-web-an1w.onrender.com/api',
   headers: {
     'Content-Type': 'application/json'
   },
